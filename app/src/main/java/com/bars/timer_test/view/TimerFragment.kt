0fb55e0ec1544c61676d21c97441770bf40a45cd
denis.bars <denys@femiglobal.com>
2022-01-09 14:36:32 +0200

    Added base connection between viewModule and the Fragment throuhg live data

--- app/src/main/java/com/bars/timer_test/view/TimerFragment.kt
+++ app/src/main/java/com/bars/timer_test/view/TimerFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bars.timer_test.R
 import com.bars.timer_test.databinding.FragmentTimerBinding
 import com.bars.timer_test.view_model.TimerViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -31,11 +34,47 @@ class TimerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribeToEvents()
+
+        timerViewModel.loadLastTimerValue(requireContext())
+    }
+
+    private fun subscribeToEvents(){
+        timerViewModel.loadingViewState.observe(viewLifecycleOwner, {
+            onLoadingFinished(it)
+        })
+        timerViewModel.dataViewState.observe(viewLifecycleOwner, {
+            onDataChanged(it)
+        })
+        timerViewModel.tickingViewState.observe(viewLifecycleOwner, {
+            onDataChanged(it)
+        })
+    }
+
+    private fun onLoadingFinished(status: Boolean){
+        binding.run {
+            progress.isVisible = false
+            startStopBtn.isVisible = true
+            timerTv.isVisible = true
+        }
+
+        if (!status) {
+            Toast.makeText(requireContext(), R.string.loading_error, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onDataChanged(value: UInt) {
+        binding.timerTv.text = convertTimeToString(value)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun convertTimeToString(value: UInt) : String{
+        //TODO implement proper convertion
+        return value.toString()
     }
 
     companion object {
--- app/src/main/java/com/bars/timer_test/view_model/TimerViewModel.kt
+++ app/src/main/java/com/bars/timer_test/view_model/TimerViewModel.kt
@@ -1,11 +1,42 @@
 package com.bars.timer_test.view_model
 
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bars.timer_test.domain.interactor.TimerUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class TimerViewModel @Inject constructor(private val timerUseCase: TimerUseCase): ViewModel() {
+class TimerViewModel @Inject constructor(private val timerUseCase: TimerUseCase) : ViewModel() {
+
+    val loadingViewState = MutableLiveData<Boolean>()
+    val dataViewState = MutableLiveData<UInt>()
+    val tickingViewState = MutableLiveData<UInt>()
+
+    fun loadLastTimerValue(context: Context) {
+        viewModelScope.launch {
+            try {
+                val timerVal = timerUseCase.loadTimer(context)
+                loadingViewState.value = true
+                dataViewState.value = timerVal
+            } catch (e: Exception) {
+                loadingViewState.value = false
+                Log.e(tag, "Failed to load data")
+            }
+        }
+    }
+
+    companion object {
+        val tag: String = TimerViewModel::class.java.simpleName
+    }
 
 }
